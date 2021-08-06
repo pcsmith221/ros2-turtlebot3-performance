310393275b3199c28ffde4c529bd6c9590badc25
Paul Smith <71151774+pcsmith221@users.noreply.github.com>
2021-08-06 14:18:23 -0400

    Delete performance directory

--- performance/scripts/default_params.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#Restores amcl parameters back to default using backup.yaml file and removes gazebo log to ensure next run can find the correct log file
-cp backup.yaml /ros_ws/install/nav2_bringup/share/nav2_bringup/params/nav2_params.yaml
-
-rm -r /root/.gazebo/log/*
--- performance/scripts/sros2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#sets security environment variables
-#arg1: true enables security, anything else disables security
-export ROS_SECURITY_ENABLE=$1
-export ROS_SECURITY_STRATEGY=Enforce
-export ROS_SECURITY_KEYSTORE=/performance/keystore
