c6324d9350fb066ebdf543b8a2d75802e0e26e3c
Paul Smith <71151774+pcsmith221@users.noreply.github.com>
2021-08-06 15:21:32 -0400

    Delete performance directory

--- performance/scripts/initial-pose.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {stamp: {sec: 0, nanosec: 0}, frame_id: "map"}, pose: { pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}, } }'
-
--- performance/scripts/sros2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export ROS_SECURITY_ENABLE=$1
-export ROS_SECURITY_STRATEGY=Enforce
-export ROS_SECURITY_KEYSTORE=/performance/keystore
