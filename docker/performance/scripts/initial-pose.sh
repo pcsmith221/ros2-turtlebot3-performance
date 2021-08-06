08cea344b724ceb7c6d16643589c1a169dce3c3a
Paul Smith <71151774+pcsmith221@users.noreply.github.com>
2021-08-06 15:48:54 -0400

    Update initial-pose.sh

--- docker/performance/scripts/initial-pose.sh
+++ docker/performance/scripts/initial-pose.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#sets pose estimate in RViz by publishing to /initialpose topic
 
 ros2 topic pub -1 /initialpose geometry_msgs/PoseWithCovarianceStamped '{ header: {stamp: {sec: 0, nanosec: 0}, frame_id: "map"}, pose: { pose: {position: {x: 0.0, y: 0.0, z: 0.0}, orientation: {w: 1.0}}, } }'
 
