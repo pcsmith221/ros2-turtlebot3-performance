4878aa95b4cfddf481701ddf33f218dbf5066314
Paul Smith <71151774+pcsmith221@users.noreply.github.com>
2021-08-06 15:36:31 -0400

    Delete CustomFoxyRobotCooperativeContainer directory

--- CustomFoxyRobotCooperativeContainer/rootfs/ros_ws/entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-source "/opt/ros/${ROS_DISTRO}/setup.bash"
-source "/ros_ws/install/setup.bash"
-exec "$@"
--- CustomFoxyRobotCooperativeContainer/rootfs/startup-vnc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-exec /bin/tini -- /usr/bin/supervisord -n
